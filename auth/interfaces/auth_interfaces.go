1871e7137d97a7ac883238cb71b010f4c866ebf6
changnoob <changnoob@outlook.com>
2022-01-07 22:53:12 +0800

    init repo structure

--- auth/interfaces/auth_interfaces.go
+++ auth/interfaces/auth_interfaces.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	authpb "github.com/changpro/disk-service/auth/deps"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 type server struct {
@@ -14,4 +15,11 @@ func NewServer() *server {
 }
 
 func (*server) RegisterNewUser(ctx context.Context,
-	req *authpb.RegisterNewUserReq)
+	req *authpb.RegisterNewUserReq) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
+func (*server) SignIn(ctx context.Context,
+	req *authpb.SignInReq) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
